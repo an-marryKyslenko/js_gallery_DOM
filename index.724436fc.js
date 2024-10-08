43f4b778c95c544513222f8bc1b57e699ccc5f28
an-marryKyslenko <olih.a@icloud.com>
2024-10-08 21:19:04 +0200

    make build

---
+++ index.724436fc.js
@@ -0,0 +1,2 @@
+var t=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");if(r){var n=r.getAttribute("href");t.setAttribute("src",n)}});
+//# sourceMappingURL=index.724436fc.js.map
